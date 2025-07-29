976576e26762119616f6ac21a452bdf9d22e8bf6
Kirsten Luz Concepcion <138779305+KirstenLuz@users.noreply.github.com>
2025-07-29 15:42:04 -0300

    Add files via upload

---
+++ fundamentos/semana1/16-input.py
@@ -0,0 +1,9 @@
+name = input("Digite o nome do filme: ")
+yearLaunch = int(input("Digite o ano de lançamento: "))
+moviePrice = float(input("Digite o preço do filme: "))
+noteRating = float(input("Digite a nota de avaliação do filme: "))
+
+print(name)
+print(yearLaunch)
+print(moviePrice)
+print(noteRating)
