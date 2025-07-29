67a8d48c78bfa62cceabbe85b2a23399db7cc860
Kirsten Luz Concepcion <138779305+KirstenLuz@users.noreply.github.com>
2025-07-29 15:35:42 -0300

    Add files via upload

---
+++ fundamentos/semana1/14-tipos.py
@@ -0,0 +1,14 @@
+name = "Barbie e o Lago dos Cisnes"
+yearLaunch = 2003
+moviePrice = 179.55
+planIncluded = False
+
+print(name)
+print(yearLaunch)
+print(moviePrice)
+print(planIncluded)
+
+print(type(name))
+print(type(yearLaunch))
+print(type(moviePrice))
+print(type(planIncluded))
